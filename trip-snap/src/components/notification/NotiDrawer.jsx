512f65d9ea98a90c2b9b948eae59f42297f99eee
yewl1110 <38202551+yewl1110@users.noreply.github.com>
2024-05-12 17:55:46 +0900

    feat: 알람 리스트 제목 수정

--- trip-snap/src/components/notification/NotiDrawer.jsx
+++ trip-snap/src/components/notification/NotiDrawer.jsx
@@ -11,6 +11,9 @@ import {
   ListItemButton,
   ListItemText,
   ListSubheader,
+  Paper,
+  Popper,
+  Typography,
 } from '@mui/material'
 import { useRouter } from 'next/navigation'
 import useFetch from '@/hooks/useFetch'
@@ -27,6 +30,9 @@ export default function NotiDrawer() {
   const { fetch } = useFetch(router)
   const { ref, inView } = useInView()
   const [fetchEnable, setFetchEnable] = useState(true)
+  const [alarmDetailId, setAlarmDetailId] = useState(null)
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
   const { data, isFetching, hasNextPage, fetchNextPage, isFetchingNextPage } =
     useInfiniteQuery({
       queryKey: ['/notification/list', fetchEnable],
@@ -82,9 +88,8 @@ export default function NotiDrawer() {
         {data?.pages.map((dataGroup, i) => (
           <React.Fragment key={i}>
             {dataGroup?.map((notification) => (
-              <>
+              <React.Fragment key={notification.id}>
                 <ListItem
-                  key={notification.id}
                   secondaryAction={
                     <IconButton
                       edge="end"
@@ -94,17 +99,57 @@ export default function NotiDrawer() {
                       <Icon>clear</Icon>
                     </IconButton>
                   }
+                  sx={{ py: 0 }}
                 >
-                  <ListItemButton disableGutters sx={{ pr: '0!important' }}>
+                  <ListItemButton
+                    disableGutters
+                    sx={{ pr: '0!important' }}
+                    onClick={(event) => {
+                      setAnchorEl(event.currentTarget)
+                      setAlarmDetailId(notification.id)
+                    }}
+                  >
                     {/* <ListItemAvatar><Avatar>A</Avatar></ListItemAvatar> */}
+
                     <ListItemText
-                      sx={{ wordBreak: 'break-all', width: 230 }}
-                      secondary={notification.title}
-                    />
+                      sx={{
+                        width: 230,
+                      }}
+                      primary={
+                        <div
+                          style={{
+                            whiteSpace: 'nowrap',
+                            wordBreak: 'break-all',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                          }}
+                        >
+                          {notification.title}
+                        </div>
+                      }
+                      secondary={notification.date}
+                    ></ListItemText>
                   </ListItemButton>
+                  <Popper
+                    id={`popover-${notification.id}`}
+                    open={notification.id === alarmDetailId}
+                    anchorEl={anchorEl}
+                    onClose={() => {
+                      setAnchorEl(null)
+                      setAlarmDetailId(null)
+                    }}
+                    placement={'left'}
+                    sx={{ zIndex: 10000 }}
+                  >
+                    <Paper sx={{ mr: 1.5, maxWidth: '300px' }}>
+                      <Typography sx={{ p: 2 }}>
+                        {notification.title}
+                      </Typography>
+                    </Paper>
+                  </Popper>
                 </ListItem>
                 <Divider variant="middle" component="li" />
-              </>
+              </React.Fragment>
             ))}
           </React.Fragment>
         ))}
